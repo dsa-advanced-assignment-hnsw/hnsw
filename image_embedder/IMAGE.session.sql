4966fda1945ebc0d79c7d50217836f1a466c8b38
huynguyen6906 <dinhhuy6906@gmail.com>
2025-11-28 23:47:00 +0700

    init SQL

---
+++ image_embedder/IMAGE.session.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS raw_image;
+
+CREATE TABLE raw_image (
+    ImageID             TEXT, 
+    Subset              TEXT, 
+    OriginalURL         TEXT, 
+    OriginalLandingURL  TEXT, 
+    License             TEXT,
+    AuthorProfileURL    TEXT, 
+    Author              TEXT, 
+    Title               TEXT, 
+    OriginalSize        TEXT, 
+    OriginalMD5         TEXT,
+    Thumbnail300KURL    TEXT, 
+    Rotation            FLOAT     
+)
+
+COPY raw_image(ImageID, Subset, OriginalURL, OriginalLandingURL, License, AuthorProfileURL, Author, Title, OriginalSize, OriginalMD5, Thumbnail300KURL, Rotation)
+FROM '/media/huynguyen/New Volume/hnsw/.cache/train-images-boxable-with-rotation.csv'   
+DELIMITER ',' 
+CSV HEADER;
+
