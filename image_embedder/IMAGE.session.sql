778c2b3bb7bb8f73db19324e5f910a740b2d4bf2
huynguyen6906 <dinhhuy6906@gmail.com>
2025-11-29 10:41:37 +0700

    Update embedder v2

--- image_embedder/IMAGE.session.sql
+++ image_embedder/IMAGE.session.sql
@@ -1,22 +1,24 @@
 DROP TABLE IF EXISTS raw_image;
 
 CREATE TABLE raw_image (
-    ImageID             TEXT, 
-    Subset              TEXT, 
-    OriginalURL         TEXT, 
-    OriginalLandingURL  TEXT, 
-    License             TEXT,
-    AuthorProfileURL    TEXT, 
-    Author              TEXT, 
-    Title               TEXT, 
-    OriginalSize        TEXT, 
-    OriginalMD5         TEXT,
-    Thumbnail300KURL    TEXT, 
-    Rotation            FLOAT     
-)
+    idx BIGSERIAL PRIMARY KEY,
+    ImageID TEXT UNIQUE NOT NULL,
+    Subset TEXT,
+    OriginalURL TEXT,
+    OriginalLandingURL TEXT,
+    License TEXT,
+    AuthorProfileURL TEXT,
+    Author TEXT,
+    Title TEXT,
+    OriginalSize TEXT,
+    OriginalMD5 TEXT,
+    Thumbnail300KURL TEXT,
+    Rotation FLOAT
+);
 
-COPY raw_image(ImageID, Subset, OriginalURL, OriginalLandingURL, License, AuthorProfileURL, Author, Title, OriginalSize, OriginalMD5, Thumbnail300KURL, Rotation)
-FROM '/media/huynguyen/New Volume/hnsw/.cache/train-images-boxable-with-rotation.csv'   
-DELIMITER ',' 
-CSV HEADER;
+COPY raw_image(ImageID, Subset, OriginalURL, OriginalLandingURL, License,
+               AuthorProfileURL, Author, Title, OriginalSize, OriginalMD5,
+               Thumbnail300KURL, Rotation)
+FROM '/media/huynguyen/New Volume/hnsw/.cache/train-images-boxable-with-rotation.csv'
+DELIMITER ',' CSV HEADER;
 
